9f17b3aa1490a92b261ba0d10edb6ed49c959eb9
tooget <tooget@gmail.com>
2018-12-24 02:23:15 +0900

    Edit/fix annotations.

--- backend/app/extensions.py
+++ backend/app/extensions.py
@@ -2,13 +2,11 @@ from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
 
-# ----[ Going to be initialized when app.__init__.register_extensions() is executed. ] ----
+# ----------------[ Going to be initialized in app.__init__.py ] --------------------------
 # Connect to DB with flask_sqlalchemy
 db = SQLAlchemy()
- 
 # Create authentificated session with JWT
 jwt = JWTManager()
-
 # Create CORS session from origin URL
 cors = CORS()
 # -----------------------------------------------------------------------------------------
