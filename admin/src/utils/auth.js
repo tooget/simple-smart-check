ef3cb48610c31dfb1497d8b5ecb69fd58d265858
Yoon <yunsu246@gmail.com>
2019-01-22 16:46:20 +0900

    UPDATE Title, Cookie Expiration Time, response.status of 503 in  admin.smartcheck.ml

--- admin/src/utils/auth.js
+++ admin/src/utils/auth.js
@@ -7,7 +7,7 @@ export function getToken() {
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token)
+  return Cookies.set(TokenKey, token, { expires: (1 / 1440) * 5 })
 }
 
 export function removeToken() {
