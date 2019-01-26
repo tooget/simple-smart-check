45316106e318c7b74fd43d372d2cddc70c693fb9
Yoon <yunsu246@gmail.com>
2019-01-26 15:17:19 +0900

    UPDATE cookie expiration time from 20 mins to 60 mins

--- frontend/www/src/helpers/auth-header.js
+++ frontend/www/src/helpers/auth-header.js
@@ -7,7 +7,7 @@ export function getToken() {
 }
 
 export function setToken(token) {
-  return Cookies.set(TokenKey, token, { expires: (1 / 1440) * 20 })
+  return Cookies.set(TokenKey, token, { expires: (1 / 1440) * 60 })
 }
 
 export function removeToken() {
